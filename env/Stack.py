2283a0f008f32fcda66322583289eb5214f91c16
alxwdm <58976179+alxwdm@users.noreply.github.com>
2020-08-07 15:19:29 +0200

    feat: rename cards_list

--- env/Stack.py
+++ env/Stack.py
@@ -8,23 +8,23 @@ class Stack():
 
     def __init__(self):
         
-        self.cards_list = list()
+        self.cards = list()
         self.points = 0
         self.power = 0
         self.type = None
         self.dragon_flag = False
 
     def _set_power(self):
-        self.power = self.cards_list[-1].power
+        self.power = self.cards[-1].power
 
     def _set_points(self):
-        self.points = sum(crds.points for crds in self.cards_list)
+        self.points = sum(crds.points for crds in self.cards)
 
     def _set_type(self):
-        self.type = self.cards_list[-1].type
+        self.type = self.cards[-1].type
 
     def _set_dragon_flag(self):
-        name_list = [crd.name for crds in self.cards_list for crd in crds.cards]
+        name_list = [crd.name for crds in self.cards for crd in crds.cards]
         if 'Dragon' in name_list:
             self.dragon_flag = True
 
@@ -36,25 +36,26 @@ class Stack():
 
     def add(self, cards_to_add):
         # add cards to stack according to game rules
-        if not self.cards_list and cards_to_add.type != 'hand':
-            self.cards_list.append(cards_to_add)
+        if not self.cards and cards_to_add.type != 'hand':
+            self.cards.append(cards_to_add)
             self._update()
             return True
         elif self.type == cards_to_add.type and self.power < cards_to_add.power:
+            # TODO: check pair_seq lengths
             if self.type == 'straight' and not(len(self.cards) == len(cards_to_add.cards)):
                 return False
             else:
-                self.cards_list.append(cards_to_add)
+                self.cards.append(cards_to_add)
                 self._update()
                 return True
         elif self.type == 'solo' and cards_to_add.type == 'solo' and cards_to_add.phoenix_flag and self.power < 15:
             old_power = self.power
-            self.cards_list.append(cards_to_add)
+            self.cards.append(cards_to_add)
             self._update()
             self.power = old_power + 0.5           
             return True   
         elif cards_to_add.type in BOMBS and self.power < cards_to_add.power:
-            self.cards_list.append(cards_to_add)
+            self.cards.append(cards_to_add)
             self._update()      
             return True
         else: # illegal move
