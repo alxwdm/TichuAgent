03de8f85434d61bc6b2b181f5a1426ee178d447e
alxwdm <58976179+alxwdm@users.noreply.github.com>
2020-08-15 13:29:10 +0200

    fix: minor bugs

--- agents/greedyAgent/greedyAgent.py
+++ agents/greedyAgent/greedyAgent.py
@@ -3,7 +3,10 @@
 
 import random
 import numpy as np
-from ../env.Cards import Cards 
+from itertools import compress
+
+from tichuagent.env.Cards import Cards 
+from tichuagent.env.Deck import Deck
 
 COMB_TYPES = {'solo': 0,
               'pair': 1,
@@ -22,26 +25,26 @@ class greedyAgent():
     def act(self, state_vec):
         # get info from state
         hand_size = state_vec[0][0]
-        hand_cards = _vec_to_cards(state_vec[0][2])
-        opp_cards_0 = _vec_to_cards(state_vec[1][2])
-        teammate_cards = _vec_to_cards(state_vec[2][2])
-        opp_cards_1 = _vec_to_cards(state_vec[3][2])
+        hand_cards = self._vec_to_cards(state_vec[0][2])
+        opp_cards_0 = self._vec_to_cards(state_vec[1][2])
+        teammate_cards = self._vec_to_cards(state_vec[2][2])
+        opp_cards_1 = self._vec_to_cards(state_vec[3][2])
         # get available combinations and types from hand cards
         # returns [solo, pair, triple, four_bomb, full, straight, straight_bomb, pair_seq]
         available_comb = hand_cards.get_available_combinations()
-        available_types = _get_available_types(available_comb)
+        available_types = self._get_available_types(available_comb)
         # pass if player has already finished
         if hand_size < 1:
-            action = _cards_to_vec(Cards([])):
+            action = self._cards_to_vec(Cards([]))
             return action
         # pass if teammate is leading
-        elif teammate_cards.power > max(opp_cards_0.power, opp_cards_1.power)
-            action = _cards_to_vec(Cards([])):
+        elif teammate_cards.power > max(opp_cards_0.power, opp_cards_1.power):
+            action = self._cards_to_vec(Cards([]))
             return action
         # if stack is empty, play lowest power of a random combination type
         elif teammate_cards.type == 'pass' and opp_cards_0.type == 'pass' and opp_cards_1.type == 'pass':
-            random_type = COMB_TYPES[random.choice(available_comb)]
-            action = _cards_to_vec(available_comb[random_type][0])
+            random_type = COMB_TYPES[random.choice(available_types)]
+            action = self._cards_to_vec(available_comb[random_type][0])
             return action
         # else try to beat opponent 
         else:
@@ -57,28 +60,28 @@ class greedyAgent():
             if available_comb[type_index]:
                 for crds in available_comb[type_index]:
                     if crds.power > leading_power:
-                        action = _cards_to_vec(crds)
+                        action = self._cards_to_vec(crds)
                         return action
                 # pass if no higher combination available
-                action = _cards_to_vec(Cards([])):
+                action = self._cards_to_vec(Cards([]))
                 return action
             # if no combination exists, try to four bomb or straight bomb
             elif available_comb[COMB_TYPES['four_bomb']]:
                 bomb = available_comb[COMB_TYPES['four_bomb']][0]
-                action = _cards_to_vec(bomb)
+                action = self._cards_to_vec(bomb)
                 return action
             elif available_comb[COMB_TYPES['straight_bomb']]:
                 bomb = available_comb[COMB_TYPES['straight_bomb']][0]
-                action = _cards_to_vec(bomb)
+                action = self._cards_to_vec(bomb)
                 return action
             # pass if opponent cannot be beaten
             else:
-                action = _cards_to_vec(Cards([])):
+                action = self._cards_to_vec(Cards([]))
                 return action
 
     def _get_available_types(self, available_comb):
         available_types = list()
-        for key, value in COMB_TYPES:
+        for key, value in COMB_TYPES.items():
             if available_comb[value]:
                 available_types.append(key)
         return available_types
