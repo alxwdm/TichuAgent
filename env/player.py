ed9f9cc457474ea08070f16d7d2b3d757b53b675
alxwdm <58976179+alxwdm@users.noreply.github.com>
2020-08-22 18:27:27 +0200

    feat: Add hand rating fn

--- env/player.py
+++ env/player.py
@@ -1,8 +1,8 @@
 # Player class for Python Implementation of Tichu
 
 import random
-from env.cards import Cards
 
+from env.cards import Cards
 
 class Player():
 
@@ -18,6 +18,7 @@ class Player():
     def assign_hand(self, cards):
         self.hand = cards
         self._update()
+        self._set_hand_rating()
         return True
 
     def remove_cards(self, cards):
@@ -65,3 +66,65 @@ class Player():
         self.hand_power = self.hand.power
         if self.hand_size == 0:
             self.finished = True
+
+    def _set_hand_rating(self):
+        """
+        This function sets the player's hand rating based on 
+        the individual cards and available combinations.
+        A high rating can be achieved if the player has 
+        a lot of high cards (Kings, Aces and Dragon or Phoenix) 
+        and a lot of good combinations (bomb, straight, triple, full).
+        """
+        good_cards = ['A', 'K', 'Dragon', 'Phoenix']
+        bad_cards = {'Dog'}
+        comb_types = {'solo': 0,
+                      'pair': 1,
+                      'triple': 2,
+                      'four_bomb': 3,
+                      'full': 4,
+                      'straight': 5,
+                      'straight_bomb': 6,
+                      'pair_seq': 7}
+
+        bombs = ['four_bomb', 'straight_bomb']
+        # initialize cards and score
+        cards = self.hand
+        score = 0
+        # update score based on individual cards
+        good_cards_list = [elem for elem in cards.cards if elem.name in good_cards]
+        for crd in good_cards_list:
+            if crd.name == 'Dragon' or crd.name == 'A':
+                score += 20
+            else:
+                score += 10
+        bad_cards_list = [elem for elem in cards.cards if elem.name in bad_cards]
+        if bad_cards_list:
+            score -= 40
+        # update score based on combinations
+        avail_combs = cards.get_available_combinations()
+        four_bombs = avail_combs[comb_types['four_bomb']]
+        if four_bombs:
+            for crds in four_bombs:
+                score += 40
+                cards.remove(crds)
+                avail_combs = cards.get_available_combinations()
+        straight_bombs = avail_combs[comb_types['straight_bomb']] 
+        if straight_bombs: 
+            for crds in straight_bombs:
+                score += 40
+                cards.remove(crds)
+                avail_combs = cards.get_available_combinations()
+        fulls = avail_combs[comb_types['full']]
+        if fulls:
+            max_fulls =  sum([full.power for full in fulls])/len(fulls)
+            score += max_fulls
+        triples = avail_combs[comb_types['triple']]
+        if triples:
+            max_triple = sum([triple.power for triple in triples])/len(triples)
+            score += max_triple
+        solos = avail_combs[comb_types['solo']]
+        straights = avail_combs[comb_types['straight']]
+        if straights:
+            max_len = max([strt.size for strt in straights])
+            score += max_len
+        self.hand_rating = score
